d084f84668d935220e7ecf636572ca0e3132f32d
yuwen <yuwen3@asiainfo.com>
2020-01-22 10:42:11 +0800

    读写锁解决多线程问题

--- src/edu/java/util/concurrent/locks/ReentrantReadWriteLockTest.java
+++ src/edu/java/util/concurrent/locks/ReentrantReadWriteLockTest.java
@@ -5,9 +5,10 @@ package edu.java.util.concurrent.locks;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * 可重入读写锁
+ * 可重入读写锁,读可并发，写串行，写会阻塞写与读
  *
  */
 public class ReentrantReadWriteLockTest {
@@ -34,25 +35,42 @@ public class ReentrantReadWriteLockTest {
 class Mycache {
     private volatile Map<String, String> map = new HashMap<>();
 
+    private ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+
     public void put(String key, String value) {
-        System.out.println(Thread.currentThread().getName() + "正在写入:" + key);
+        rwLock.writeLock().lock();
         try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + "正在写入:" + key);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "完成写入:" + key);
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            rwLock.writeLock().unlock();
         }
-        map.put(key, value);
-        System.out.println(Thread.currentThread().getName() + "完成写入:" + key);
+
     }
 
     public void get(String key) {
-        System.out.println(Thread.currentThread().getName() + "正在读取:" + key);
+        rwLock.readLock().lock();
         try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + "正在读取:" + key);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String value = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "完成读取:" + value);
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            rwLock.readLock().unlock();
         }
-        String value = map.get(key);
-        System.out.println(Thread.currentThread().getName() + "完成读取:" + value);
     }
 }
